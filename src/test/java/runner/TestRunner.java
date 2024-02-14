5defacba4aa5c7346d6f33a6f18eaa7609edc929
Waqas Khan <waqas.khan@codegator.us>
2024-02-13 21:29:56 -0500

    added login scenario

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,5 +1,12 @@
 package runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/Features",glue = "stepDefinition", dryRun = false, monochrome = true,plugin = "pretty")
+@RunWith(Cucumber.class)
 public class TestRunner {
 
 }
--- src/test/java/stepDefinition/LoginTest.java
+++ src/test/java/stepDefinition/LoginTest.java
@@ -1,5 +1,44 @@
 package stepDefinition;
 
-public class LoginTest {
+import org.junit.Assert;
+import org.openqa.selenium.By;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import utilities.Base;
+
+public class LoginTest extends Base {
+
+	@Given("I open chrome browser")
+	public void i_open_chrome_browser() {
+		openBrowser();
+	}
+
+	@Given("I visit the environment")
+	public void i_visit_the_environment() {
+		driver.get("https://login.salesforce.com");
+	}
+
+	@Given("I enter username")
+	public void i_enter_username() {
+		driver.findElement(By.id("username")).sendKeys("batch09@codegator.us");
+	}
+
+	@Given("I enter password")
+	public void i_enter_password() {
+		driver.findElement(By.id("password")).sendKeys("Welcome1");
+	}
+
+	@When("I click the login button")
+	public void i_click_the_login_button() {
+		driver.findElement(By.id("Login")).click();
+	}
+
+	@Then("I should see the home page")
+	public void i_should_see_the_home_page() {
+		boolean present = driver.findElement(By.cssSelector("a[title='Home Tab']")).isDisplayed();
+		Assert.assertTrue(present);
+	}
 
 }
