8d32c155b7b78b6dc936b8b648b9727393359aee
Waqas Khan <waqas.khan@codegator.us>
2024-02-15 18:58:44 -0500

    removed some code

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,9 +5,12 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/Features",glue = "stepDefinition", dryRun = false, monochrome = true,plugin={"pretty",
-"html:target/cucumber-reports/cucumber-html",
-"json:target/cucumber-reports/cucumber.json"})
+@CucumberOptions(
+		features = "src/test/resources/Features",
+		glue = "stepDefinition", 
+		dryRun = false, 
+		monochrome = true,
+		plugin="pretty")
 @RunWith(Cucumber.class)
 public class TestRunner {
 
