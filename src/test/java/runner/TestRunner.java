a2ab3de64ea74607771425e9b71c6db387779edd
Waqas Khan <wuikhan@gmail.com>
2024-02-15 19:23:24 -0500

    updated the runner to create cucumber files

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,17 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/Features",glue = "stepDefinition", dryRun = true, monochrome = true,plugin = "pretty")
+@CucumberOptions(
+		features = "src/test/resources/Features",
+		glue = "stepDefinition", 
+		dryRun = false, 
+		monochrome = true,
+		plugin = {
+				"pretty",
+				"html:target/cucumber-reports/cucumber.html",
+				"json:target/cucumber-reports/cucumber.json"
+		}
+		)
 @RunWith(Cucumber.class)
 public class TestRunner {
 
--- src/test/resources/Features/Login.feature
+++ src/test/resources/Features/Login.feature
@@ -9,3 +9,10 @@ Feature: Test Login functionality
     When I click the login button
     Then I should see the home page
 
+  Scenario: Verify the standard user credentials can login
+    Given I open chrome browser
+    And I visit the environment
+    And I enter "john@codegator.us" username
+    And I enter "Welcome2" password
+    When I click the login button
+    Then I should see the home page
